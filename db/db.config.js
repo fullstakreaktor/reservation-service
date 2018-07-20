145af4ee459db608b2e0ec0fe7bbfe2b730d1948
administrator <administrator@Users-MacBook-Pro-2.local>
2018-07-20 14:46:12 -0700

    change db config

--- db/db.config.js
+++ db/db.config.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-	host: 'http://127.0.0.1:3003',
-	user: 'FILL_ME_IN',
-	password: 'FILL_ME_IN',
-	database: 'airpnp'
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'airpnp',
 });
 
 module.exports = db;
