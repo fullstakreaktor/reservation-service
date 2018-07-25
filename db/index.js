8a25475759d3d1c3d78480523d1223ad3b3a61f9
Kony Pham <coniekp@icloud.com>
2018-07-24 18:10:30 -0700

    Add db methods

--- db/index.js
+++ db/index.js
@@ -1,7 +1,37 @@
 const db = require('./db.config.js');
 
 
-db.connect((err, response) => {
-  if (err) console.log(err);
-  else console.log(`SUCCESS : ${response}`);
-});
+const getListingById = (id, callback) => {
+  let queryStr = 'SELECT * from listings WHERE id = ?';
+  db.query(queryStr, id, callback);
+};
+
+const getBookedDatesByListingId = (id, callback) => {
+  let queryStr = 'SELECT check_in, check_out FROM booked_dates WHERE listing_id = ?';
+  db.query(queryStr, id, callback);
+};
+
+const postNewBookedDates = (data, callback) => {
+  let queryStr = 'INSERT INTO booked_dates (listing_id, check_in, check_out) VALUES (?, ?, ?)';
+  db.query(queryStr, [data.listingId, data.checkIn, data.checkOut], callback);
+};
+
+const postNewReservation = (data, callback) => {
+  let queryStr = 'INSERT INTO reservations ' + 
+  	'(guest_id, booked_dates_id, total_adults, total_children, total_infants, total_charge) VALUES ?)';
+  db.query(queryStr, [[data.guestId, data.bookedDatesId, data.adults, data.children, data.infants, data.total]], callback);
+};
+
+const deleteBookedDatesById = (id, callback) => {
+  let queryStr = 'DELETE FROM booked_dates WHERE id = (?)';
+  db.query(queryStr, id, callback);
+}
+
+module.exports = {
+	getListingById,
+	getBookedDatesByListingId,
+	postNewBookedDates,
+	postNewReservation,
+	deleteBookedDatesById
+}
+
