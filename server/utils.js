136801b875ebb2afd5d25de0185ca389a9685ed0
Kony Pham <coniekp@icloud.com>
2018-07-25 01:20:30 -0700

    add get/post request handlers

---
+++ server/utils.js
@@ -0,0 +1,9 @@
+const parseBookedDates = (data) => {
+	data.checkIn = new Date(...data.checkIn);
+	data.checkOut = new Date(...data.checkOut);
+	return data;
+}
+
+module.exports = {
+	parseBookedDates,
+}
