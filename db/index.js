4516a0f6cd981424fa069c28ae3ee8dc24db4027
Kony Pham <coniekp@icloud.com>
2018-07-25 17:55:53 -0700

    delint files

--- db/index.js
+++ db/index.js
@@ -2,37 +2,37 @@ const db = require('./db.config.js');
 
 
 const getListingById = (id, callback) => {
-  let queryStr = 'SELECT * from listings WHERE id = ?';
+  const queryStr = 'SELECT * from listings WHERE id = ?';
   db.query(queryStr, id, callback);
 };
 
 const getBookedDatesByListingId = (id, callback) => {
-  let queryStr = 'SELECT check_in, check_out FROM booked_dates WHERE listing_id = ?';
+  const queryStr = 'SELECT check_in, check_out FROM booked_dates WHERE listing_id = ?';
   db.query(queryStr, id, callback);
 };
 
 const postNewBookedDates = (data, callback) => {
-  let queryStr = 'INSERT INTO booked_dates (listing_id, check_in, check_out) VALUES (?)';
+  const queryStr = 'INSERT INTO booked_dates (listing_id, check_in, check_out) VALUES (?)';
   db.query(queryStr, [[data.listingId, data.checkIn, data.checkOut]], callback);
 };
 
-const postNewReservation = (data, callback) => {
-  let queryStr = 'INSERT INTO reservations ' + 
-  	'(guest_id, booked_dates_id, total_adults, total_children, total_infants, total_charge) VALUES (?)';
-  db.query(queryStr, [[data.guestId, data.bookedDatesId, data.adults, data.children, data.infants, data.total]], callback);
+const postNewReservation = ({guestId, bookedDatesId, guests, total}, callback) => {
+  const queryStr = 'INSERT INTO reservations '
+    + '(guest_id, booked_dates_id, total_adults, total_children, total_infants, total_charge) VALUES (?)';
+  const values = [guestId, bookedDatesId, guests.adults, guests.children, guests.total, total];
+  db.query(queryStr, [values], callback);
 };
 
 const deleteBookedDatesById = (id, callback) => {
-  let queryStr = 'DELETE FROM booked_dates WHERE id = ?';
+  const queryStr = 'DELETE FROM booked_dates WHERE id = ?';
   db.query(queryStr, id, callback);
-}
+};
 
 
 module.exports = {
-	getListingById,
-	getBookedDatesByListingId,
-	postNewBookedDates,
-	postNewReservation,
-	deleteBookedDatesById
-}
-
+  getListingById,
+  getBookedDatesByListingId,
+  postNewBookedDates,
+  postNewReservation,
+  deleteBookedDatesById,
+};
