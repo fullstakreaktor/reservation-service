c17952f614e1fe363cc6b965a973a90ed561bf7e
Kony Pham <coniekp@icloud.com>
2018-07-25 22:47:58 -0700

    Add rating sprite

--- client/components/ListingSnippet.jsx
+++ client/components/ListingSnippet.jsx
@@ -3,10 +3,13 @@ import React from 'react';
 var ListingSnippet = (props) => (
   <div className="snippet-container">
     <div className="row">
-      <div className="rate">$10</div>
+      <div className="rate">${props.listing.rate}</div>
       <div>  per night</div>
     </div>
-    <div className="rating"></div>
+    <div className="row">
+      <img className="rating paw-1-half" src="./img/ratings-sprite.png" />
+      <div className="ratings-count">{props.listing.ratingsCount}</div>
+     </div>
   </div>
 );
 
