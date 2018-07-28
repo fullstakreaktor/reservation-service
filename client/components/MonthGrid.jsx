7f7d4296f99383a6401c81115258dfba639d698f
Kony Pham <coniekp@icloud.com>
2018-07-27 17:11:58 -0700

    Implement month grid

---
+++ client/components/MonthGrid.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Day from './Day.jsx';
+
+var MonthGrid = (props) => {
+  const days = [];
+  for (let i = 0; i < props.startDay - 1; i++){
+  	days.push(<Day key={-i} date={null} type={"unavailableDates"} />);
+  }
+
+  for (let i = 1; i <= props.monthLength; i++) {
+    days.push(<Day key={i} clickHandler={props.clickHandler} date={i}  type={props.unavailableDates.includes(i)? "unavailable-date": "available-date"}/>)
+  }
+
+  return (
+  	<div>
+	  	<div className="week-header">
+		    <div>Su</div>
+		    <div>Mo</div>
+		    <div>Tu</div>
+		    <div>We</div>
+		    <div>Th</div>
+		    <div>Fr</div>
+		    <div>Sa</div>
+		</div>
+    	<div className="month-grid">{days}</div>
+    </div>
+  )
+};
+
+
+export default MonthGrid;
