d7beea85794211de35645448878c0f8eeb9561d1
Kony Pham <coniekp@icloud.com>
2018-07-30 14:32:16 -0700

    setup testing env

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,22 @@
 module.exports = {
-	module: {
-		rules: [{
-			test:/\.jsx$/,
-			exclude: /node_modules/,
-			use: {
-				loader = 'babel-loader'
-			}
-		}],
-		entry: __dirname + '/client/index/jsx',
-		output: {
+	module: 
+	{
+		loaders: 
+		[
+			{loader: 'style-loader!css-loader', test:/\.css$/},
+			{loader: 'babel-loader', include: [__dirname + '/client'], test:/\.jsx?$/},
+			{loader: 'url-loader', test:/\.png$/}
+		],
+		entry: __dirname + '/client/index.jsx',
+		output: 
+		{
 			filename: 'bundle.js',
 			path: __dirname + '/public'
+		}, 
+		resolve: 
+		{
+			extensions: ['.js', '.jsx', '.jsx.html'],
+			modules: [__dirname + '/node_modules']
 		}
 	}
 }
