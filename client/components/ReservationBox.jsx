314833439bf8ff226ca3c204c6e9257f75b61c76
Kony Pham <coniekp@icloud.com>
2018-08-02 18:42:47 -0700

    Add ajax get request for listing info

--- client/components/ReservationBox.jsx
+++ client/components/ReservationBox.jsx
@@ -9,21 +9,78 @@ class ReservationBox extends React.Component {
     super(props);
     this.state = {
       listing: {
-        id: 10,
-        rate: 10,
-        avgRating: 2.4,
-        ratingsCount: 88,
-        weeklyViews: 222
+        id: 33,
+        hostId: 33,
+        rate: 33,
+        minStay: 3,
+        maxGuests: 3,
+        fees: 3.33,
+        taxRate: 3,
+        avgRating: 3.3,
+        ratingsCount: 33,
+        weeklyViews: 333,
+
       },
+      hasDates: false
     };
   }
+
+  componentDidMount() {
+    this.getListingInfo();
+  }
+  getListingInfo() {
+    let url = `/api/listings/${this.props.listingId}`;
+    fetch(url)
+    .then(res => res.json())
+    .then((res) => this.setListingDetails(res))
+    .catch(err => console.log(err))
+  }
+
+  handleDatesSelect () {
+    this.setState({
+      hasDates: true
+    })
+  }
+
+  handleDatesReset () {
+    this.setState ({
+      hasDates: false
+    })
+  }
+
+  setListingDetails (data) {
+    let listing = {
+      hostId: data.hostId, 
+      rate: data.rate,
+      reviewsCount: data.reviews.total_reviews,
+      avgRating: data.reviews.avg_rating,
+      minStay: data.min_stay,
+      maxGuests: data.max_guests,
+      fees: data.fees,
+      taxRate: data.tax_rate,
+      weeklyViews: data.weekly_views
+    }
+    this.setState({
+      listing: listing
+    }, () => console.log(this.state))
+  }
+
 
   render() {
     return (
       <div className="container">
-        <ListingSnippet listing={this.state.listing} />
-        <ReservationDetails listing={this.state.listing}/>
-        <Promo views={this.state.weeklyViews} hasSetDates={false} />
+        <ListingSnippet 
+          listing={this.state.listing} 
+        />
+        <ReservationDetails 
+          listing={this.state.listing} 
+          onDatesSelect={this.handleDatesSelect.bind(this)} 
+          onClearDates={this.handleDatesReset.bind(this)}
+        />
+        <Promo 
+          views={this.state.listing.weeklyViews} 
+          hasDates={this.state.hasDates} 
+        />
       </div>
     );
   }
--- db/db.js
+++ db/db.js
@@ -9,37 +9,49 @@ const db = mysql.createConnection({
 });
 
 
-const getListingById = (id, callback) => {
-  const queryStr = 'SELECT * from listings WHERE id = ?';
-  db.query(queryStr, id, callback);
+const getListingById = ({listingId}, callback) => {
+  const queryStr = `SELECT * from listings WHERE id = ?`;
+  db.query(queryStr, listingId, callback);
 };
 
-const getBookedDatesByListingId = (id, callback) => {
-  const queryStr = 'SELECT check_in, check_out FROM booked_dates WHERE listing_id = ?';
-  db.query(queryStr, id, callback);
+const getBookedDatesByListingId = ([listingId, year, month], callback) => {
+  let startDate = [year, month, 1].join('-');
+  let endDate = month === 12? [Number(year)+1, 1, 1].join('-'): [year, Number(month)+1, 1].join('-');
+
+  const queryStr = `SELECT check_in, check_out FROM booked_dates WHERE listing_id = ? AND check_in >= ? AND check_in < ? ORDER BY check_in`;
+  db.query(queryStr, [listingId, startDate, endDate], callback);
 };
+
+const getFirstBookedDateAfterTarget = ([listingId, year, month, date], callback) => {
+  let startDate = [year, month, date].join('-');
+  let endDate = month === 12? [Number(year)+1, 1, 1].join('-'): [year, Number(month)+1, 1].join('-');
+
+  const queryStr = `SELECT check_in FROM booked_dates WHERE listing_id = ? AND check_in > ? AND check_in < ? ORDER BY check_in ASC LIMIT 1`;
+  db.query(queryStr, [listingId, startDate, endDate], callback)
+}
 
 const postNewBookedDates = (data, callback) => {
-  const queryStr = 'INSERT INTO booked_dates (listing_id, check_in, check_out) VALUES (?)';
-  db.query(queryStr, [[data.listingId, data.checkIn, data.checkOut]], callback);
-};
+  const queryStr = `INSERT INTO booked_dates (listing_id, check_in, check_out) VALUES (?)`;
+  db.query(queryStr, [data.listingId, data.checkIn, data.checkOut], callback);
+}
 
 const postNewReservation = ({guestId, bookedDatesId, guests, total}, callback) => {
-  const queryStr = 'INSERT INTO reservations '
-    + '(guest_id, booked_dates_id, total_adults, total_pups, total_charge) VALUES (?)';
-  const values = [guestId, bookedDatesId, guests.adults, guests.pups, total];
+  const queryStr = `INSERT INTO reservations `
+    + `(guest_id, booked_dates_id, total_adults, total_children, total_infants, total_charge) VALUES (?)`;
+  const values = [guestId, bookedDatesId, guests.adults, guests.children, guests.total, total];
   db.query(queryStr, [values], callback);
 };
 
-const deleteBookedDatesById = (id, callback) => {
-  const queryStr = 'DELETE FROM booked_dates WHERE id = ?';
-  db.query(queryStr, id, callback);
+const deleteBookedDatesById = ({listingId}, callback) => {
+  const queryStr = `DELETE FROM booked_dates WHERE id = ?`;
+  db.query(queryStr, listingId, callback);
 };
 
 
 module.exports = {
   getListingById,
   getBookedDatesByListingId,
+  getFirstBookedDateAfterTarget,
   postNewBookedDates,
   postNewReservation,
   deleteBookedDatesById,
